6f62cfe8014139d22389f0f10a3eb1b11e50a875
Frank Cabada <frankcabada@yahoo.com>
2017-04-01 15:53:32 -0400

    New TestClass

--- TestClass.java
+++ TestClass.java
@@ -1,27 +1,15 @@
-package edu.umd.cs.findbugs.detect;
-
 import org.apache.bcel.classfile.Code;
 
-import edu.umd.cs.findbugs.BugAccumulator;
 import edu.umd.cs.findbugs.BugReporter;
 import edu.umd.cs.findbugs.BugInstance;
 
-public class TestClass {
+public class TestClass extends ByteCodeScanningDetector {
 
-	protected final BugAccumulator bugAccumulator;
+	private BugReporter reporter;
 
-	public TestClass(BugReporter bugReporter) {
-		this.bugAccumulator = new BugAccumulator(bugReporter);
+	@Override
+	public void visit(Field field) {
+		if (field.isStatic()) {
+			reporter.reportBug(new BugInstance(this, "TEST_BUG", NORMAL_PRIORITY))
+		}
 	}
-
-	public void visit(Code code) {
-		accumulateBug();
-
-		bugAccumulator.reportAccumulatedBugs();
-	}
-
-	protected void accumulateBug() {
-		bugAccumulator.accumulateBug(new BugInstance(this, "TEST_BUG",
-			NORMAL_PRIORITY).addClassAndMethod(this), this);
-	}
-}
