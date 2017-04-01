2e17cc203cf58a76f64dbe572454be5407e9a73c
Frank Cabada <frankcabada@yahoo.com>
2017-04-01 16:38:30 -0400

    Continued testing

--- TestClass.java
+++ TestClass.java
@@ -6,10 +6,18 @@ import edu.umd.cs.findbugs.BugInstance;
 public class TestClass extends ByteCodeScanningDetector {
 
 	private BugReporter reporter;
+	private String currentClass;
+
+	@Override
+	public void visit(JavaClass someObj) {
+		currentClass = someObj.getClassName();
+		super.visit(someObj);
+	}
 
 	@Override
 	public void visit(Field field) {
-		if (field.isStatic()) {
-			reporter.reportBug(new BugInstance(this, "TEST_BUG", NORMAL_PRIORITY))
+		super.visit(field);
+		if (!field.isStatic()) {
+			reporter.reportBug(new BugInstance(this, "TEST", NORMAL_PRIORITY).addClass(currentClass).addField(currentClass, field.getName(), field.getSignature(), true));
 		}
 	}
